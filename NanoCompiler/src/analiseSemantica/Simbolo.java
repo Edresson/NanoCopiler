8d7b8e97582564a7099d877f82d3ad215f55448e
Fernando Leite <Fernando Leite@DESKTOP-H2T87I0.home>
2017-07-31 23:32:12 -0300

    dasdsa

--- NanoCompiler/src/analiseSemantica/Simbolo.java
+++ NanoCompiler/src/analiseSemantica/Simbolo.java
@@ -1,10 +1,38 @@
 package analiseSemantica;
 
 import parser.SimpleNode;
+import parser.Token;
 
 public class Simbolo {
 	
 	private SimpleNode node;
 	private Object value;
+	private Token token;
+	private int type;
+	
+	public SimpleNode getNode() {
+		return node;
+	}
+	public void setNode(SimpleNode node) {
+		this.node = node;
+	}
+	public Object getValue() {
+		return value;
+	}
+	public void setValue(Object value) {
+		this.value = value;
+	}
+	public Token getToken() {
+		return token;
+	}
+	public void setToken(Token token) {
+		this.token = token;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
 	
 }
